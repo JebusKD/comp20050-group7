2ee35bf72fc118b7fd249d716007c3bd6584491f
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-13 10:15:23 +0000

    initialiserhombusgrid, initialiseoctagongrid

--- src/userinterface/QuaxUserInterface.java
+++ src/userinterface/QuaxUserInterface.java
@@ -1,5 +1,6 @@
 package userinterface;
 
+import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -9,11 +10,41 @@ public class QuaxUserInterface {
 
 	private Stage stage;
 	
-	private OctagonTile[][] octagonGrid;
-	private RhombusTile[][] rhombusGrid;
+	private OctagonTile[][] octagonGridCells;
+	private RhombusTile[][] rhombusGridCells;
+	
+	private GridPane octagonGrid;
+	private GridPane rhombusGrid;
 	
 	public QuaxUserInterface(Stage stage) {
 		this.stage = stage;
+		
+		initialiseOctagonGrid();
+		initialiseRhombusGrid();
+	}
+	
+	private void initialiseOctagonGrid() {
+		octagonGridCells = new OctagonTile[11][11];
+		octagonGrid = new GridPane();
+		for (int i = 0; i < 11; i++) {
+			for (int j = 0; j < 11; j++) {
+				OctagonTile newTile = new OctagonTile();
+				octagonGridCells[i][j] = newTile;
+				octagonGrid.add(newTile, i, j);
+			}
+		}
+	}
+	
+	private void initialiseRhombusGrid() {
+		rhombusGridCells = new RhombusTile[11][11];
+		rhombusGrid = new GridPane();
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				RhombusTile newTile = new RhombusTile();
+				rhombusGridCells[i][j] = newTile;
+				rhombusGrid.add(newTile, i, j);
+			}
+		}
 	}
 	
 	private static interface Tile {
