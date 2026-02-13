4b88e811480d6cfbec157fdcf1f0f445b91f8d1b
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-13 10:03:02 +0000

    QuaxTileColour

--- src/userinterface/QuaxUserInterface.java
+++ src/userinterface/QuaxUserInterface.java
@@ -1,11 +1,31 @@
 package userinterface;
 
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import types.QuaxTileColour;
+
 public class QuaxUserInterface {
 
 	private Stage stage;
 	
+	private OctagonTile[][] octagonGrid;
+	private RhombusTile[][] rhombusGrid;
+	
 	public QuaxUserInterface(Stage stage) {
 		this.stage = stage;
+	}
+	
+	private static interface Tile {
+		public void setColour(QuaxTileColour colour);
+	}
+	
+	private static class OctagonTile extends Polygon implements Tile {
+		
+	}
+	
+	private static class RhombusTile extends Rectangle implements Tile {
+		
 	}
 	
 }
