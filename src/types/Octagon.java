ebd0f7c35676b09ee4aee1cb8d27cc1e5dc5361e
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-17 18:14:39 +0000

    Clean up unused Octagon methods

--- src/types/Octagon.java
+++ src/types/Octagon.java
@@ -10,42 +10,8 @@ public class Octagon extends QuaxTile {
 		this.yPos = y;
 	}
 	
-	public Octagon(String coordinates) {
-		int[] coords = stringToCoords(coordinates);
-		this.xPos = coords[0];
-		this.yPos = coords[1];
-	}
-	
-	public String getCoordinates() {
-		return coordsToString(xPos, yPos);
-	}
-	
-	public static int[] stringToCoords(String coords) {
-		int[] pair = new int[2];
-		pair[0] = coords.charAt(0) - 'a';
-		pair[1] = Integer.parseInt(coords.substring(1));
-		return pair;
-	}
-	
-	public static String coordsToString(int x, int y) {
-		if (y < 1 || y > 11) throw new IllegalArgumentException("y must be in range [0, 10]");
-		
-		String returnStr = switch (x) {
-		case 0 -> "a";
-		case 1 -> "b";
-		case 2 -> "c";
-		case 3 -> "d";
-		case 4 -> "e";
-		case 5 -> "f";
-		case 6 -> "g";
-		case 7 -> "h";
-		case 8 -> "i";
-		case 9 -> "j";
-		case 10 -> "k";
-		default -> throw new IllegalArgumentException("x must be in range [0,10]");
-		};
-		
-		return returnStr + (y+1);
+	public QuaxCoordinate getCoordinates() {
+		return new QuaxCoordinate(xPos, yPos, true);
 	}
 	
 	@Override
@@ -53,8 +19,8 @@ public class Octagon extends QuaxTile {
 		if (isFree()) return false;
 		
 		if (getColour() == QuaxTileColour.BLACK)
-			return yPos == 0;
-		else return xPos == 0;
+			return yPos == 10;
+		else return xPos == 10;
 	}
 	
 	@Override
@@ -62,8 +28,8 @@ public class Octagon extends QuaxTile {
 		if (isFree()) return false;
 		
 		if (getColour() == QuaxTileColour.BLACK)
-			return yPos == 10;
-		else return xPos == 10;
+			return yPos == 0;
+		else return xPos == 0;
 	}
 		
 }
