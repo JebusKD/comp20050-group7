f6b564f2a7124435ede0a94d3a963957d333e163
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-17 08:33:21 +0000

    Board

---
+++ src/model/Board.java
@@ -0,0 +1,27 @@
+package model;
+
+import types.Octagon;
+import types.Rhombus;
+
+public class Board {
+
+	private Octagon[][] octagonGrid;
+	private Rhombus[][] rhombusGrid;
+	
+	public Board() {
+		this.octagonGrid = new Octagon[11][11];
+		this.rhombusGrid = new Rhombus[10][10];
+		
+		for (int i = 0; i < 11; i++) {
+			for (int j = 0; j < 11; j++) {
+				octagonGrid[i][j] = new Octagon(i, j);
+			}
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				rhombusGrid[i][j] = new Rhombus(i, j);
+			}
+		}
+	}
+}
