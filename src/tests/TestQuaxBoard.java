0222262b7eda7840581b1c65273c479554c3c9aa
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-20 23:45:32 +0000

    QuaxBoard Tests

--- src/tests/TestQuaxBoard.java
+++ src/tests/TestQuaxBoard.java
@@ -8,8 +8,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.QuaxBoard;
+import types.QuaxCoordinate;
+import types.QuaxTileColour;
+
 class TestQuaxBoard {
 
+	private QuaxBoard b;
+	
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
@@ -20,6 +26,7 @@ class TestQuaxBoard {
 
 	@BeforeEach
 	void setUp() throws Exception {
+		b = new QuaxBoard();
 	}
 
 	@AfterEach
@@ -27,8 +34,45 @@ class TestQuaxBoard {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testWinningBoard1() {
+		for (int i = 0; i < 11; i++) {
+			b.makeMove(new QuaxCoordinate(i, 3, true), QuaxTileColour.WHITE);
+		}
+		assertTrue(b.checkForWinningMove());
+	}
+	
+	@Test
+	void testWinningBoard2() {
+		for (int i = 0; i < 11; i++) {
+			b.makeMove(new QuaxCoordinate(6, i, true), QuaxTileColour.BLACK);
+		}
+		assertTrue(b.checkForWinningMove());
+	}
+	
+	// Not going the full way should result in no win
+	@Test
+	void testWinningBoard3() {
+		for (int i = 0; i < 10; i++) {
+			b.makeMove(new QuaxCoordinate(i, 3, true), QuaxTileColour.WHITE);
+		}
+		assertFalse(b.checkForWinningMove());
+	}
+	
+	@Test
+	void testWinningBoard4() {
+		for (int i = 0; i < 10; i++) {
+			b.makeMove(new QuaxCoordinate(6, i, true), QuaxTileColour.BLACK);
+		}
+		assertFalse(b.checkForWinningMove());
+	}
+	
+	// Feature that prevents moves after the game is won.
+	@Test
+	void testWinningBoardInvalidMove() {
+		for (int i = 0; i < 11; i++) {
+			b.makeMove(new QuaxCoordinate(6, i, true), QuaxTileColour.BLACK);
+		}
+		assertFalse(b.validMove(new QuaxCoordinate(8, 8, true), QuaxTileColour.BLACK));
 	}
 
 }
