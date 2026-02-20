b63101b854ff5a0feeb41eb44e443379ed13a417
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-20 21:54:20 +0000

    fix onHigh and onLow for white

--- src/types/Octagon.java
+++ src/types/Octagon.java
@@ -20,7 +20,7 @@ public class Octagon extends QuaxTile {
 		
 		if (getColour() == QuaxTileColour.BLACK)
 			return yPos == 10;
-		else return xPos == 10;
+		else return xPos == 0;
 	}
 	
 	@Override
@@ -29,7 +29,7 @@ public class Octagon extends QuaxTile {
 		
 		if (getColour() == QuaxTileColour.BLACK)
 			return yPos == 0;
-		else return xPos == 0;
+		else return xPos == 10;
 	}
 		
 }
