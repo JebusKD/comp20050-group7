6167da1c7cd92e43b3bfb32d7977ce198f87527c
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-20 22:55:25 +0000

    testQuaxBoard in testsuite

--- src/tests/Sprint1TestSuite.java
+++ src/tests/Sprint1TestSuite.java
@@ -9,7 +9,8 @@ import org.junit.platform.suite.api.Suite;
 	TestQuaxCoordinate.class,
 	TestOctagon.class,
 	TestRhombus.class,
-	TestQuaxTileGroup.class
+	TestQuaxTileGroup.class,
+	TestQuaxBoard.class
 })
 @DisplayName("Sprint 1 Unit Tests")
 class Sprint1TestSuite {
