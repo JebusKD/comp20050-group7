241f157e54379dc18a9a18f751e5f036c52363db
Jamie Kildea <jamiekildea@hotmail.com>
2026-02-20 16:35:36 +0000

    Reinstate 1px Octagon Gap
    Didn't work for all resolutions on 0.

--- src/userinterface/QuaxUserInterface.java
+++ src/userinterface/QuaxUserInterface.java
@@ -34,7 +34,7 @@ public class QuaxUserInterface {
 
 	private static final double OCTAGON_WIDTH = 60;
 	
-	private static final double OCTAGON_GRID_GAP = 0;
+	private static final double OCTAGON_GRID_GAP = 1;
 	private static final double RHOMBUS_GRID_GAP = calculateRhombusGridGap(OCTAGON_GRID_GAP, OCTAGON_WIDTH);
 
 	private Stage stage;
