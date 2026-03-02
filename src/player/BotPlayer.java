528fc7fca3305604e2beb2b51e6820d08f51f335
Jamie Kildea <jamiekildea@hotmail.com>
2026-03-02 07:01:10 +0000

    BotPlayer multithread

--- src/player/BotPlayer.java
+++ src/player/BotPlayer.java
@@ -1,14 +1,24 @@
 package player;
 
+import javafx.stage.Stage;
 import model.QuaxBoard;
+import types.QuaxCoordinate;
+import types.QuaxTileColour;
 
 public abstract class BotPlayer extends QuaxPlayer {
 
+	public BotPlayer(QuaxTileColour colour, Stage stage) {
+		super(colour, stage);
+	}
+	
 	protected abstract QuaxCoordinate computeMove(QuaxBoard b);
 	
 	@Override
-	public abstract void movePrompt(QuaxBoard b) {
-		submitMove(computeMove(b));
+	public void movePrompt(QuaxBoard b) {
+		Thread t = new Thread(() -> {
+			submitMove(computeMove(b));
+		});
+		t.start();
 	}
 	
 }
