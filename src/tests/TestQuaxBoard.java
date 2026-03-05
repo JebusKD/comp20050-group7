59268bbb55c26bedd7edbada727194f7098add2b
Jamie Kildea <jamiekildea@hotmail.com>
2026-03-05 08:37:14 +0000

    Clean up quaxBoard tests

--- src/tests/TestQuaxBoard.java
+++ src/tests/TestQuaxBoard.java
@@ -15,22 +15,10 @@ import types.QuaxTileColour;
 class TestQuaxBoard {
 
 	private QuaxBoard b;
-	
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
 
 	@BeforeEach
 	void setUp() throws Exception {
 		b = new QuaxBoard();
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
 	}
 
 	@Test
