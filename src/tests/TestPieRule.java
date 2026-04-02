2118f4359733a99140d6fac10b5c310d1855854f
Jamie Kildea <jamiekildea@hotmail.com>
2026-04-02 12:48:45 +0100

    test pie rule

--- src/tests/TestPieRule.java
+++ src/tests/TestPieRule.java
@@ -14,19 +14,48 @@ import types.QuaxTileColour;
 class TestPieRule {
 
 	QuaxController controller;
+	QuaxPlayer p1;
+	QuaxPlayer p2;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		QuaxPlayer p1 = new HumanPlayer("Player 1", QuaxTileColour.BLACK);
-		QuaxPlayer p2 = new HumanPlayer("Player 2", QuaxTileColour.WHITE);
+		p1 = new HumanPlayer("Player 1", QuaxTileColour.BLACK);
+		p2 = new HumanPlayer("Player 2", QuaxTileColour.WHITE);
 		controller = new QuaxController(p1, p2);
 	}
 	
 	@Test
-	void testPieRule() {
+	void testPieRuleSuccess() {
 		controller.makeMove(new QuaxCoordinate(3, 3, true));
 		controller.doPieRule();
-		assertEquals(2, controller.getBoard().getMoveNumber());
 		assertEquals(QuaxTileColour.WHITE, controller.curPlayer().getColour());
+		assertEquals(p1, controller.curPlayer());
 	}
+	
+	@Test
+	void testPieRuleFailure1() {
+		controller.doPieRule();
+		assertEquals(QuaxTileColour.BLACK, controller.curPlayer().getColour());
+		assertEquals(p1, controller.curPlayer());
+	}
+	
+	@Test
+	void testPieRuleFailure2() {
+		controller.makeMove(new QuaxCoordinate(3, 3, true));
+		controller.makeMove(new QuaxCoordinate(3, 4, true));
+		controller.doPieRule();
+		assertEquals(QuaxTileColour.BLACK, controller.curPlayer().getColour());
+		assertEquals(p1, controller.curPlayer());
+	}
+	
+	@Test
+	void testPieRuleFailure3() {
+		controller.makeMove(new QuaxCoordinate(3, 3, true));
+		controller.makeMove(new QuaxCoordinate(3, 4, true));
+		controller.makeMove(new QuaxCoordinate(3, 5, true));
+		controller.doPieRule();
+		assertEquals(QuaxTileColour.WHITE, controller.curPlayer().getColour());
+		assertEquals(p2, controller.curPlayer());
+	}
+
 }
