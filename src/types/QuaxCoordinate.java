54bcab4d78a3d2a65d9992edba4ccd6891c89d72
Jamie Kildea <jamiekildea@hotmail.com>
2026-04-02 12:27:48 +0100

    QuaxCoordinate todo

--- src/types/QuaxCoordinate.java
+++ src/types/QuaxCoordinate.java
@@ -5,6 +5,8 @@ public class QuaxCoordinate {
 	private final int y;
 	private final boolean octagonMove;
 	
+	// TODO Flag argument, see clean code "Error Handling"
+	// Change this constructor to private and construct via static methods
 	public QuaxCoordinate(int x, int y, boolean octagonMove) {
 		this.x = x;
 		this.y = y;
