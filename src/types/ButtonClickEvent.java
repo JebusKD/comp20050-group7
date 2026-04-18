b0e5ce1e4035c451851575ae38800bc06f18853c
Sock_06 <senan.oconnor@ucdconnect.ie>
2026-04-18 14:05:38 +0100

    Some formatting

--- src/types/ButtonClickEvent.java
+++ src/types/ButtonClickEvent.java
@@ -6,12 +6,11 @@ import javafx.event.EventType;
 public class ButtonClickEvent extends Event {
 	public static final EventType<ButtonClickEvent> PIE_RULE_CLICKED_EVENT = new EventType<>("pieRuleClickedEvent");	
 
-	//TODO - Remove?
+	// TODO - Remove unused constant
 	//private static final long serialVersionUID = 1L;
 	
 	public ButtonClickEvent(EventType<ButtonClickEvent> t) {
 		super(t);
 	}
-	
 }
 	
