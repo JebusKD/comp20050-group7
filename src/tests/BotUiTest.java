f0d0380e9f13385fc3d333e66e13a84d0e17c0ba
Sock_06 <senan.oconnor@ucdconnect.ie>
2026-04-22 10:10:53 +0100

    Renamed Variables

--- src/tests/BotUiTest.java
+++ src/tests/BotUiTest.java
@@ -1,25 +1,16 @@
 package tests;
 
-import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
-import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.testfx.util.WaitForAsyncUtils;
 import controller.QuaxController;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.matcher.base.WindowMatchers.isShowing;
-import  org.testfx.api.FxAssert;
-import model.QuaxBoard;
+
 import player.BotPlayer;
-import player.HumanPlayer;
-import types.QuaxCoordinate;
-import types.QuaxTile;
 import types.QuaxTileColour;
 
 
@@ -40,10 +31,10 @@ public class BotUiTest {
     	// Bot should automatically make a move if possible, so should always be the human's turn.
     	assertFalse(controller.curPlayer() instanceof BotPlayer);
     	if (controller.curPlayer().getColour() == QuaxTileColour.BLACK) { // Human goes first
-    		assertEquals(0,controller.getBoard().getMoveNumber()); //robot has not moved
+    		assertEquals(0,controller.getQuaxBoard().getMoveNumber()); //robot has not moved
     	}
     	else { // Otherwise, bot moves first and makes exactly one move.
-    		assertEquals(1,controller.getBoard().getMoveNumber());
+    		assertEquals(1,controller.getQuaxBoard().getMoveNumber());
     	}
   
     }
@@ -56,10 +47,10 @@ public class BotUiTest {
     	if (controller.curPlayer().getColour() == QuaxTileColour.BLACK) { // Human goes first
     		robot.clickOn("#octagon5-5");
     		WaitForAsyncUtils.waitForFxEvents();
-    		assertEquals(2,controller.getBoard().getMoveNumber()); //robot went after human
+    		assertEquals(2,controller.getQuaxBoard().getMoveNumber()); //robot went after human
     	}
 		else {
-            assertEquals(1,controller.getBoard().getMoveNumber()); //robot is BLACK so has moved
+            assertEquals(1,controller.getQuaxBoard().getMoveNumber()); //robot is BLACK so has moved
         }
     }
 }
