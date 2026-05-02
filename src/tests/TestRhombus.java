0a8742b57a788d8621974d27472a6413be51496e
Sock_06 <senan.oconnor@ucdconnect.ie>
2026-05-02 15:27:24 +0100

    Corrected test names, said OctagonCoordX. Other small changes

--- src/tests/TestRhombus.java
+++ src/tests/TestRhombus.java
@@ -2,7 +2,6 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import types.Rhombus;
@@ -13,56 +12,56 @@ import types.QuaxTileColour;
 class TestRhombus {
 
 	@Test
-	void testOctagonCoordinates1() {
+	void testRhombusCoordinates1() {
 		QuaxTile t = new Rhombus(5, 2);
 		QuaxCoordinate q = t.getCoordinates();
 		assertEquals(5, q.x());
 	}
 	
 	@Test
-	void testOctagonCoordinates2() {
+	void testRhombusCoordinates2() {
 		QuaxTile t = new Rhombus(1, 0);
 		QuaxCoordinate q = t.getCoordinates();
 		assertEquals(1, q.x());
 	}
 	
 	@Test
-	void testOctagonCoordinates3() {
+	void testRhombusCoordinates3() {
 		QuaxTile t = new Rhombus(8, 7);
 		QuaxCoordinate q = t.getCoordinates();
 		assertEquals(8, q.x());
 	}
 	
 	@Test
-	void testOctagonCoordinates4() {
+	void testRhombusCoordinates4() {
 		QuaxTile t = new Rhombus(0, 7);
 		QuaxCoordinate q = t.getCoordinates();
 		assertEquals(0, q.x());
 	}
 	
 	@Test
-	void testOctagonCoordinates5() {
+	void testRhombusCoordinates5() {
 		QuaxTile t = new Rhombus(5, 2);
 		QuaxCoordinate q = t.getCoordinates();
 		assertEquals(2, q.y());
 	}
 	
 	@Test
-	void testOctagonCoordinates6() {
+	void testRhombusCoordinates6() {
 		QuaxTile t = new Rhombus(1, 0);
 		QuaxCoordinate q = t.getCoordinates();
 		assertEquals(0, q.y());
 	}
 	
 	@Test
-	void testOctagonCoordinates7() {
+	void testRhombusCoordinates7() {
 		QuaxTile t = new Rhombus(8, 7);
 		QuaxCoordinate q = t.getCoordinates();
 		assertEquals(7, q.y());
 	}
 	
 	@Test
-	void testOctagonCoordinates8() {
+	void testRhombusCoordinates8() {
 		QuaxTile t = new Rhombus(0, 7);
 		QuaxCoordinate q = t.getCoordinates();
 		assertEquals(7, q.y());
@@ -70,40 +69,41 @@ class TestRhombus {
 
 
 	@Test
-	void testIsFree1() {
+	void testRhombusIsFree1() {
 		QuaxTile t = new Rhombus(5, 5);
 		t.setTileColour(QuaxTileColour.BLACK);
 		assertFalse(t.isFree());
 	}
 	
 	@Test
-	void testIsFree2() {
+	void testRhombusIsFree2() {
 		QuaxTile t = new Rhombus(5, 5);
 		t.setTileColour(QuaxTileColour.WHITE);
 		assertFalse(t.isFree());
 	}
 	
 	@Test
-	void testIsFree3() {
+	void testRhombusIsFree3() {
 		QuaxTile t = new Rhombus(5, 5);
 		t.setTileColour(QuaxTileColour.NONE);
 		assertTrue(t.isFree());
 	}
 	
 	@Test
-	void testIsFree4() {
+	void testRhombusIsFree4() {
 		QuaxTile t = new Rhombus(5, 5);
 		assertTrue(t.isFree());
 	}
-	
+
+
 	@Test
-	void testUnassignedGroup() {
+	void testRhombusUnassignedGroup() {
 		QuaxTile t = new Rhombus(5, 5);
 		assertNull(t.getTileGroup());
 	}
 	
 	@Test
-	void testStrategyValue() {
+	void testRhombusStrategyValue() {
 		QuaxTile t = new Rhombus(3, 2);
 		t.setStrategyValue(6);
 		assertEquals(6, t.getStrategyValue());
