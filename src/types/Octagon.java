2ef184c569831c67dcc3f9872a30836b338e4af7
Jamie Kildea <jamiekildea@hotmail.com>
2026-05-03 21:46:20 +0100

    Octagon error checks

--- src/types/Octagon.java
+++ src/types/Octagon.java
@@ -1,5 +1,6 @@
 package types;
 
+import static model.QuaxBoard.NUM_OCTAGONS;
 import model.QuaxBoard;
 
 
@@ -9,22 +10,40 @@ import model.QuaxBoard;
 public class Octagon extends QuaxTile {
 
 	public Octagon(int x, int y) {
+		if (x < 0 || x >= NUM_OCTAGONS) {
+			throw new IllegalArgumentException("Octagon must be constructed with x coordinate in range [0," + (NUM_OCTAGONS-1) + "].");
+		}
+		if (y < 0 || y >= NUM_OCTAGONS) {
+			throw new IllegalArgumentException("Octagon must be constructed with y coordinate in range [0," + (NUM_OCTAGONS-1) + "].");
+		}
+		
 		super(x, y);
 	}
 	
 	public Octagon(Octagon o) {
+		if (o == null) {
+			throw new IllegalArgumentException("Octagon cannot be constructed using null Octagon as copy.");
+		}
+		
 		super(o);
 	}
 
 
 	@Override
 	public QuaxCoordinate getCoordinates() {
+		assert xPosition >= 0 && xPosition < NUM_OCTAGONS && yPosition >= 0 && yPosition < NUM_OCTAGONS;
+		
 		return QuaxCoordinate.newOctagonCoordinate(xPosition, yPosition);
 	}
 
 
 	public int distanceToLowWall() {
-		assert isOccupied();
+		assert xPosition >= 0 && xPosition < NUM_OCTAGONS && yPosition >= 0 && yPosition < NUM_OCTAGONS;
+		
+		if (isFree()) {
+			throw new IllegalStateException("Distance to Low Wall cannot be evaluated for free Octagons.");
+		}
+		
 		if (getTileColour() == QuaxTileColour.BLACK) {
 			return yPosition;
 		}
@@ -34,7 +53,12 @@ public class Octagon extends QuaxTile {
 	}
 
 	public int distanceToHighWall() {
-		assert isOccupied();
+		assert xPosition >= 0 && xPosition < NUM_OCTAGONS && yPosition >= 0 && yPosition < NUM_OCTAGONS;
+		
+		if (isFree()) {
+			throw new IllegalStateException("Distance to High Wall cannot be evaluated for free Octagons.");
+		}
+		
 		if (getTileColour() == QuaxTileColour.BLACK) {
 			return QuaxBoard.NUM_OCTAGONS - (yPosition + 1);
 		}
