c9b994801f76704ce0ac64e66042d66d90bec81b
Jamie Kildea <jamiekildea@hotmail.com>
2026-05-04 14:49:31 +0100

    QuaxEventHandler error checks

--- src/userinterface/QuaxEventHandler.java
+++ src/userinterface/QuaxEventHandler.java
@@ -13,9 +13,23 @@ import static types.ButtonClickEvent.*;
 public class QuaxEventHandler {
 
     public static void setupTileClickEvents(QuaxController controller, Stage stage) {
+    	if (controller == null) {
+    		throw new IllegalArgumentException("QuaxEventHandler cannot be setup for null QuaxController.");
+    	}
+    	if (stage == null) {
+    		throw new IllegalArgumentException("QuaxEventHandler cannot be setup for null Stage.");
+    	}
+    	
         stage.addEventHandler(TILE_CLICKED_EVENT, new EventHandler<>() {
             @Override
             public void handle(QuaxCoordinateEvent coords) {
+            	if (coords == null) {
+            		throw new IllegalArgumentException("TILE_CLICKED_EVENT cannot be called for null QuaxCoordinate.");
+            	}
+            	if (controller.currentPlayer() == null) {
+            		throw new IllegalStateException("TILE_CLICKED_EVENT cannot be called with uninitialised players.");
+            	}
+            	
                 if (controller.currentPlayer() instanceof HumanPlayer) {
                     controller.attemptMove(coords.coordinate());
                 }
@@ -24,9 +38,20 @@ public class QuaxEventHandler {
     }
 
     public static void setupButtonEvents(QuaxController controller, Stage stage) {
+    	if (controller == null) {
+    		throw new IllegalArgumentException("QuaxEventHandler cannot be setup for null QuaxController.");
+    	}
+    	if (stage == null) {
+    		throw new IllegalArgumentException("QuaxEventHandler cannot be setup for null Stage.");
+    	}
+    	
         stage.addEventHandler(PIE_RULE_CLICKED_EVENT, new EventHandler<>() {
             @Override
             public void handle(ButtonClickEvent event) {
+            	if (controller.currentPlayer() == null) {
+            		throw new IllegalStateException("TILE_CLICKED_EVENT cannot be called with uninitialised players.");
+            	}
+            	
                 if (controller.currentPlayer() instanceof HumanPlayer) {
                     controller.doPieRule();
                 }
