e79b8decae8a3a128a44d74115b6fc59d830d666
Jamie Kildea <jamiekildea@hotmail.com>
2026-05-04 15:52:49 +0100

    QuaxBoard error checking

--- src/model/QuaxBoard.java
+++ src/model/QuaxBoard.java
@@ -35,6 +35,10 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 
 	public QuaxBoard(QuaxBoard copyBoard) {
+		if (copyBoard == null) {
+			throw new IllegalArgumentException("Cannot create board copy from null QuaxBoard.");
+		}
+		
 		this.octagonGrid = new Octagon[NUM_OCTAGONS][NUM_OCTAGONS];
 		this.rhombusGrid = new Rhombus[NUM_RHOMBUSES][NUM_RHOMBUSES];
 		
@@ -49,6 +53,8 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 
 	private void initialiseGrids() {
+		assert octagonGrid != null && rhombusGrid != null;
+		
 		for (int i = 0; i < NUM_OCTAGONS; i++) {
 			for (int j = 0; j < NUM_OCTAGONS; j++) {
 				this.octagonGrid[i][j] = new Octagon(i, j);
@@ -63,6 +69,9 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 
 	private void initialiseGrids(QuaxBoard copyBoard) {
+		assert copyBoard != null &&
+				octagonGrid != null && rhombusGrid != null;
+		
 		for (int i = 0; i < NUM_OCTAGONS; i++) {
 			for (int j = 0; j < NUM_OCTAGONS; j++) {
 				this.octagonGrid[i][j] = new Octagon(copyBoard.getOctagon(i, j));
@@ -77,6 +86,8 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 
 	private void initialiseGroups(QuaxBoard copyBoard) {
+		assert copyBoard != null;
+		
 		for (QuaxTileGroup g : copyBoard.trackedGroups) {
 			QuaxTileGroup newGroup = new QuaxTileGroup();
 			GroupManager gm = new GroupManager();
@@ -90,14 +101,34 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 
 
 	public Octagon getOctagon(int x, int y) {
+		assert octagonGrid != null;
+		if (x < 0 || x >= NUM_OCTAGONS) {
+			throw new IllegalArgumentException("getOctagon must be called for x coordinate in range [0," + NUM_OCTAGONS + "]. Was " + x + ".");
+		}
+		if (y < 0 || y >= NUM_OCTAGONS) {
+			throw new IllegalArgumentException("getOctagon must be called for y coordinate in range [0," + NUM_OCTAGONS + "]. Was " + y + ".");
+		}
+		
 		return this.octagonGrid[x][y];
 	}
 	
 	public Rhombus getRhombus(int x, int y) {
+		assert rhombusGrid != null;
+		if (x < 0 || x >= NUM_RHOMBUSES) {
+			throw new IllegalArgumentException("getRhombus must be called for x coordinate in range [0," + NUM_RHOMBUSES + "]. Was " + x + ".");
+		}
+		if (y < 0 || y >= NUM_RHOMBUSES) {
+			throw new IllegalArgumentException("getRhombus must be called for y coordinate in range [0," + NUM_RHOMBUSES + "]. Was " + y + ".");
+		}
+		
 		return this.rhombusGrid[x][y];
 	}
 	
 	public QuaxTile getTile(QuaxCoordinate coord) {
+		if (coord == null) {
+			throw new IllegalArgumentException("getTile cannot be called for null coordiante.");
+		}
+		
 		if (coord.isOctagon()) {
 			return getOctagon(coord.x(), coord.y());
 		}
@@ -107,6 +138,10 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 	
 	public QuaxTileColour getTileColour(QuaxCoordinate coord) {
+		if (coord == null) {
+			throw new IllegalArgumentException("getTileColour cannot be called for null coordiante.");
+		}
+		
 		return getTile(coord).getTileColour();
 	}
 
@@ -134,11 +169,19 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 
 	private QuaxTileGroup previousGroup() {
+		assert previousMove != null;
 		return getTile(previousMove).getTileGroup();
 	}
 
 
 	public boolean validMove(QuaxCoordinate coord, QuaxTileColour colour) {
+		if (coord == null) {
+			throw new IllegalArgumentException("Cannot check validity of move for null coordinate.");
+		}
+		if (colour == null || colour == QuaxTileColour.NONE) {
+			throw new IllegalArgumentException("Cannot check validity of move for " + colour + " colour.");
+		}
+		
 		if (!checkForWinningMove() && (coord.isOctagon() || isValidRhombusPlacement(coord, colour))) {
 			return getTile(coord).isFree();
 		}
@@ -147,6 +190,13 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 
 	public boolean validMove(QuaxTile tile) {
+		if (tile == null) {
+			throw new IllegalArgumentException("Cannot check validity of move for null tile.");
+		}
+		if (tile.tileDoesNotExist()) {
+			throw new IllegalArgumentException("Cannot check validity of move for non-existing tile.");
+		}
+		
 		return validMove(tile.getCoordinates(), currentColourTurn());
 	}
 
@@ -158,6 +208,7 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 			result = QuaxTileColour.BLACK;
 		}
 		else {
+			assert previousMove() != null;
 			result = getTileColour(previousMove()).flip();
 		}
 
@@ -173,6 +224,13 @@ public class QuaxBoard implements Iterable<QuaxTile> {
     }
 	
 	public boolean isValidRhombusPlacementForBothPlayers(QuaxCoordinate coord) {
+		if (coord == null) {
+			throw new IllegalArgumentException("Cannot check validity of rhombus placement for null coordinates.");
+		}
+		if (coord.isOctagon()) {
+			throw new IllegalArgumentException("Cannot check validity of rhombus placement for Octagon coordinates.");
+		}
+		
 		return isValidRhombusPlacement(coord, QuaxTileColour.BLACK)
 				&& isValidRhombusPlacement(coord, QuaxTileColour.WHITE);
 	}
@@ -194,6 +252,13 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 
 
 	public void makeMove(QuaxCoordinate coordinate, QuaxTileColour colour) {
+		if (coordinate == null) {
+			throw new IllegalArgumentException("Cannot make move onto null coordinate.");
+		}
+		if (colour != QuaxTileColour.BLACK && colour != QuaxTileColour.WHITE) {
+			throw new IllegalArgumentException("Cannot make move to " + colour + " colour.");
+		}
+		
 		QuaxTile tile;
 		GroupManager moveManager = new GroupManager();
 
@@ -211,6 +276,13 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 
 	public void makeMove(QuaxTile t) {
+		if (t == null) {
+			throw new IllegalArgumentException("Cannot make move with null tile.");
+		}
+		if (t.tileDoesNotExist()) {
+			throw new IllegalArgumentException("Cannot make move with non-existing tile.");
+		}
+		
 		makeMove(t.getCoordinates(), currentColourTurn());
 	}
 	
@@ -222,15 +294,21 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	private class GroupManager {
 
 		private void trackGroup(QuaxTileGroup g) {
+			assert g != null && g.groupExists();
+			
 			trackedGroups.addFirst(g);
 		}
 
 		private void untrackGroup(QuaxTileGroup g) {
+			assert g != null && g.groupExists();
+			
 			trackedGroups.remove(g);
 		}
 
 
 		private void assignGroup(QuaxTile newTile) {
+			assert newTile != null && newTile.tileExists();
+			
 			QuaxTileColour c = newTile.getTileColour();
 			assert c != QuaxTileColour.NONE;
 			
@@ -242,6 +320,8 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 
 
 		private ArrayList<QuaxTileGroup> getAdjacentGroups(QuaxTile[][] neighbours, QuaxTileColour colour) {
+			assert neighbours != null && (colour == QuaxTileColour.BLACK || colour == QuaxTileColour.WHITE);
+			
 			ArrayList<QuaxTileGroup> nearbyGroups = new ArrayList<>(MAX_ADJACENT_TILE_GROUPS);
 
 			for (QuaxTile[] tileArray : neighbours) {
@@ -258,11 +338,15 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 		}
 
 		private boolean tileNotMemberOfGroup(ArrayList<QuaxTileGroup> groups, QuaxTile t) {
+			assert groups != null && t != null && t.tileExists();
+			
 			return !(groups.contains(t.getTileGroup()));
 		}
 
 
 		private void expandLargestGroup(QuaxTile tile, ArrayList<QuaxTileGroup> adjacentGroups) {
+			assert tile != null && tile.tileExists() && adjacentGroups != null;
+			
 			if (adjacentGroups.isEmpty()) {
 				trackGroup(new QuaxTileGroup(tile));
 			}
@@ -274,6 +358,8 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 		}
 
 		private QuaxTileGroup getBiggestGroup(ArrayList<QuaxTileGroup> adjGroups) {
+			assert adjGroups != null;
+			
 			int maxSize = -1;
 			QuaxTileGroup biggestGroup = null;
 
@@ -288,6 +374,9 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 		}
 
 		private void mergeNearbyGroups(QuaxTileGroup largest, ArrayList<QuaxTileGroup> adjacentGroups) {
+			assert largest != null && largest.groupExists()
+					&& adjacentGroups != null;
+			
 			for (QuaxTileGroup g : adjacentGroups) {
 				if (g != largest) {
 					largest.merge(g);
@@ -302,11 +391,22 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 
 	/* Use NeighbourFinder class for strategy and group handling */
 	public QuaxTile[][] getNeighbours(QuaxCoordinate centreCoord) {
+		if (centreCoord == null) {
+			throw new IllegalArgumentException("Centre Coordinate cannot be null.");
+		}
+		
 		NeighbourFinder nf = new NeighbourFinder(this, centreCoord);
 		return nf.getCoordinateNeighbours();
 	}
 
 	public List<QuaxTile> getNeighboursList(QuaxTile centreTile) {
+		if (centreTile == null) {
+			throw new IllegalArgumentException("getNeighboursList cannot be run for null centre tile.");
+		}
+		if (centreTile.tileDoesNotExist()) {
+			throw new IllegalArgumentException("getNeighboursList cannot be run for non-existing centre tile.");
+		}
+		
 		LinkedList<QuaxTile> result = new LinkedList<>();
 
 		for (QuaxTile[] arr : getNeighbours(centreTile.getCoordinates())) {
@@ -321,6 +421,10 @@ public class QuaxBoard implements Iterable<QuaxTile> {
 	}
 	
 	public QuaxTile[][] getSquareOfOctagonNeighbours(Octagon o) {
+		if (o == null) {
+			throw new IllegalArgumentException("getSquareOfOctagonNeighbours cannot be run for null Octagon.");
+		}
+		
 		NeighbourFinder nf = new NeighbourFinder(this, o.getCoordinates());
 		return nf.getSquareOfAdjacentOctagonNeighbours();
 	}
